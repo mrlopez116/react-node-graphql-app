afd0fb366f8ebf2ccdc4a0cae63b86ea7063826a
Mario Lopez <mariolopez@Marios-MacBook-Pro-2.local>
2020-01-25 11:32:09 -0800

    Basic non-database graphql working

--- app.js
+++ app.js
@@ -1,11 +1,41 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphQlhttp = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res, next) => {
-	res.send('Hello World!');
-});
+// Congiuring graphql api
+app.use(
+	'/graphql',
+	graphQlhttp({
+		schema    : buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+
+        type RootMutation {
+            createEvent(name: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `), // Points to a valid graphql Schema
+		rootValue : {
+			events      : () => {
+				return [ 'Sometehing 1', 'Somethign 2', 'Something 3' ];
+			},
+			createEvent : (args) => {
+				const eventName = args.name; // name because that ishow we neamed it in the    `RootMutation`
+				return eventName;
+			},
+			graphiql    : true // we get nice user interface
+		}
+	})
+);
+
 app.listen(8081);
